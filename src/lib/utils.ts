4175d3e4e1045a052fc6bed929895a5635f4379f
davidwilliam700211-sketch <davidwilliam700211@gmail.com>
2025-09-04 17:35:46 +0100

    N$F change

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,9 @@
-import { clsx } from "clsx";
+import { clsx, ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 /**
  * Utility function to conditionally merge Tailwind + custom class names.
- * Example:
- *   cn("px-4", isActive && "bg-pink-600", "text-white")
  */
-export function cn(...inputs: any[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
